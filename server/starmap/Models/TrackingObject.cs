08f441437490bad4fb24b2b2dd343511409befa0
emagnca <magnus.carlhammar@systemvaruhuset.se>
2013-07-03 14:02:33 +0200

    Server updates

---
+++ server/starmap/Models/TrackingObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Starcounter;
+
+namespace starmap.Model
+{
+    [Database]
+    class TrackingObject
+    {
+        public string name;
+        public DateTime lastUpdate;
+    }
+}
--- server/starmap/Start.cs
+++ server/starmap/Start.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using Starcounter;
+using starmap.Model;
 
 class Start
 {
     static void Main()
     {
-        Console.WriteLine("Hello World!!");
+        Db.Transaction(() =>
+        {
+            var o = new TrackingObject() { FirstName = "Albert", LastName = "Einstein" };
+            new Quote() { Person = albert, Text = "Make things as simple as possible, but not simpler" };
+        });
     }
 }
