708611f3f1d7efaee4980f0a2ee4bdfd0149f31a
eingmey <ingvar.meyer@systemvaruhuset.se>
2013-09-04 14:14:58 +0200

    Starcounter multi thread test 01

---
+++ StarcounterMultiThreadTest01/StarcounterMultiThreadTest01/Model/DbAdministration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Starcounter;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarcounterMultiThreadTest01.Model
+{
+    class DbAdministration
+    {
+        public void DbInsert(long i, string firstname, string lastname)
+        {
+            Db.Transaction(() =>
+            {
+                new Person() { Id = i, FirstName = firstname, LastName = lastname };
+            });
+        }
+
+        public Person DbGetPerson(long persId)
+        {
+            Person p = null;
+            Db.Transaction(() =>
+            {
+                p = (Person)Db.SQL("SELECT P FROM Person P WHERE P.Id=?", persId).First;
+            });
+            return p;
+        }
+
+        public void DbDelete()
+        {
+            Db.Transaction(delegate()
+            {
+                foreach (Person p in Db.SQL("SELECT p FROM Person p"))
+                    p.Delete();
+
+            });
+        }
+
+    }
+}
